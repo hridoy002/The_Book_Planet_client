898bcb24e113eb9dc9968ce8fc159e5343e863c5
hridoy002 <hridoyalhasan2@gmail.com>
2022-05-14 03:14:25 +0600

    removing

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import {getAuth} from "firebase/auth"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBkwr9lsjDQoZcaMwxzQzgQ9cM58ZsLpwE",
-  authDomain: "the-book-planet.firebaseapp.com",
-  projectId: "the-book-planet",
-  storageBucket: "the-book-planet.appspot.com",
-  messagingSenderId: "354571735296",
-  appId: "1:354571735296:web:e04d553bb2ffeaedc7e3e5"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
